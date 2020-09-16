d2a10733da15d56d50eb78849f4f5413c5c416d3
Isorethan <dessout.yannick@gmail.com>
2020-09-16 23:31:31 +0200

    javascript for intra section navigation

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
 const links = document.querySelectorAll("#main-nav ul a");
+const sections = document.querySelectorAll("section")
+const divs = document.querySelectorAll('section > div')
  
 for (const link of links) {
   link.addEventListener("click", clickHandler);
@@ -12,3 +14,27 @@ function clickHandler(e) {
     behavior: "smooth"
   });
 }
+
+
+
+
+for (const section of sections) {
+  let targetSection =  section.querySelectorAll("h2");
+ let subMenu = document.createElement('ul');
+ subMenu.className = "sub-menu" ;
+   for (h2 of targetSection) {
+    let subLi = document.createElement("li");
+    subLi.innerText = h2.innerText ;
+    subMenu.appendChild(subLi);
+    subLi.className = "sub-li" ;
+   }
+ 
+ 
+  
+
+ 
+  divs.appendChild(subMenu)
+ 
+
+}
+
