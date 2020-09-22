5fc719e091ab1f05d0f101662a3b70d8c0485ec3
isorethan <isorethan@gmail.com>
2020-09-22 10:06:39 +0200

    added Keyframes and photos

--- assets/app.js
+++ assets/app.js
@@ -1,40 +1,56 @@
 const links = document.querySelectorAll("#main-nav ul a");
-const sections = document.querySelectorAll("section")
-const divs = document.querySelectorAll('section > div')
+const mainNav = document.getElementById("main-nav")
+const sections = document.querySelectorAll("section");
+const divs = document.querySelectorAll('section.subcontainer');
+const header = document.getElementById("header");
+const articles = document.querySelectorAll( "section > article")
+const visibleArticles = document.querySelectorAll(' section.visible > article')
  
+
+AOS.init();
+
 for (const link of links) {
   link.addEventListener("click", clickHandler);
 }
  
+
+window.addEventListener("scroll" , function(){
+  const body = document.querySelector('body')
+  console.log(window.scrollY)
+})
+
+
+
+
+
 function clickHandler(e) {
   e.preventDefault();
+  header.classList.remove('visible')
   const href = this.getAttribute("href");
- 
-  document.querySelector(href).scrollIntoView({
-    behavior: "smooth"
-  });
-}
-
-
-
-
-for (const section of sections) {
-  let targetSection =  section.querySelectorAll("h2");
- let subMenu = document.createElement('ul');
- subMenu.className = "sub-menu" ;
-   for (h2 of targetSection) {
-    let subLi = document.createElement("li");
-    subLi.innerText = h2.innerText ;
-    subMenu.appendChild(subLi);
-    subLi.className = "sub-li" ;
-   }
- 
- 
+  console.log("I got cliked !"+href)
+  for (section of sections) {  
+    let sectionId = "#"+section.id ;
+    if (href === sectionId ) {
+    
+      section.parentElement.classList.add('visible')
+      document.querySelector(href).scrollIntoView({
+        behavior: "instant"
+      });
+    } else {
+      section.parentElement.classList.remove('visible')
+    }
+  }
   
-
- 
-  divs.appendChild(subMenu)
- 
-
+  
+  
 }
+
+
+if (header.classList.contains('visible')) {
+  mainNav.classList.remove("transparent");
+}
+
+ 
+
+
 
